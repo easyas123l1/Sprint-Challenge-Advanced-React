e3ccb4900729fcdc92191d269a890d4815b0ea62
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-11-01 10:32:12 -0500

    first 2 mvp complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import PlayerCard from './components/playerCard/PlayerCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    players: []
+  }
+
+  axiosFetch = () => {
+    axios.get(`http://localhost:5000/api/players`)
+    .then(results => {
+      console.log(results);
+      this.setState({ players: results.data })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  componentDidMount() {
+    this.axiosFetch();
+  }
+  //player data has country id name and searches
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.players.map(player => (
+            <PlayerCard player={player} key={player.id} />
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
